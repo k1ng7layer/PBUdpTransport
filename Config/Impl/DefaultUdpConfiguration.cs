729edd95a4b1837f146d9aa0b92da591e3704e79
Paata Baliashvili <pl.baliashvili@gmail.com>
2023-08-16 01:08:16 +0300

    add default configuration values. add constructor without config arguments

---
+++ Config/Impl/DefaultUdpConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace PBUdpTransport.Config.Impl
+{
+    public class DefaultUdpConfiguration : IUdpConfiguration
+    {
+        public int MTU { get; set; } = 1032;
+        public int MaxPacketResendCount { get; set; } = 2;
+        public int ReceiveBufferSize { get; set; } = 1032;
+    }
+}
